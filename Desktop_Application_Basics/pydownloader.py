741e3a0140a1b9dd51dc056cc4e954c59d4470c4
Pondi Brian <pondibrian@gmail.com>
2018-12-08 20:58:18 +0300

    Functionalities and Signal

--- Desktop_Application_Basics/pydownloader.py
+++ Desktop_Application_Basics/pydownloader.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import urllib.request
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
@@ -9,22 +10,22 @@ class Downloader(QDialog):
 
         layout =QVBoxLayout()
 
-        url = QLineEdit()
-        save_location = QLineEdit()
-        progress = QProgressBar()
+        self.url = QLineEdit()
+        self.save_location = QLineEdit()
+        self.progress = QProgressBar()
         download = QPushButton("Download")
 
         #placeholder text
-        url.setPlaceholderText("URL to Download from")
-        save_location.setPlaceholderText("File save location")
+        self.url.setPlaceholderText("URL to Download from")
+        self.save_location.setPlaceholderText("File save location")
 
-        progress.setValue(0)
-        progress.setAlignment(Qt.AlignCenter)
+        self.progress.setValue(0)
+        self.progress.setAlignment(Qt.AlignCenter)
 
 
-        layout.addWidget(url)
-        layout.addWidget(save_location)
-        layout.addWidget(progress)
+        layout.addWidget(self.url)
+        layout.addWidget(self.save_location)
+        layout.addWidget(self.progress)
         layout.addWidget(download)
 
         #Important
@@ -35,8 +36,20 @@ class Downloader(QDialog):
         self.setWindowIcon(QIcon('download_icon.png'))
         self.setFocus()
 
-       
-   
+        #Event handlers ---= SIGNALS
+        download.clicked.connect(self.download)
+
+    def download(self):
+        url =self.url.text()
+        save_location = self.save_location.text()
+        urllib.request.urlretrieve(url,save_location,self.report)
+
+    def report (self, blocknum, blocksize, totalsize):
+        readsofar = blocknum * blocksize
+        if totalsize > 0:
+            percent = readsofar *100 /totalsize
+            self.progress.setValue(int(percent))
+
 
 app = QApplication(sys.argv)
 dialog = Downloader()
