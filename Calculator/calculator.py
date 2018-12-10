023710d318f32d2b46af3c766ee36bc2b0048c7f
Pondi Brian <pondibrian@gmail.com>
2018-12-10 08:39:36 +0300

    buttons displaying

---
+++ Calculator/calculator.py
@@ -0,0 +1,50 @@
+import sys
+from PyQt4 import QtCore, QtGui
+from ui import main
+
+
+class Calculator_Class(main.Ui_MainWindow, QtGui.QMainWindow):
+    def __init__(self):
+        super(Calculator_Class, self).__init__()
+        self.setupUi(self)
+
+        self.pb_0.clicked.connect(lambda: self.display_screen('0'))
+        self.pb_1.clicked.connect(lambda: self.display_screen('1'))
+        self.pb_2.clicked.connect(lambda: self.display_screen('2'))
+        self.pb_3.clicked.connect(lambda: self.display_screen('3'))
+        self.pb_4.clicked.connect(lambda: self.display_screen('4'))
+        self.pb_5.clicked.connect(lambda: self.display_screen('5'))
+        self.pb_6.clicked.connect(lambda: self.display_screen('6'))
+        self.pb_7.clicked.connect(lambda: self.display_screen('7'))
+        self.pb_8.clicked.connect(lambda: self.display_screen('8'))
+        self.pb_9.clicked.connect(lambda: self.display_screen('9'))
+
+        self.pb_add.clicked.connect(lambda: self.display_screen(' + '))
+        self.pb_minus.clicked.connect(lambda: self.display_screen(' - '))
+        self.pb_divide.clicked.connect(lambda: self.display_screen(' / '))
+        self.pb_multiply.clicked.connect(lambda: self.display_screen(' * '))
+
+        self.pb_clear.clicked.connect(self.screen_LE.clear)
+        self.pb_back.clicked.connect(self.screen_LE.backspace)
+
+    def display_screen(self, value):
+        self.screen_LE.insert(value)
+
+    def calculation(self, val1, val2, operator):
+        val1 = float(val1)
+        val2 = float(val2)
+        if operator is 'add':
+            return val1+val2
+        elif operator is 'sub':
+            return val1-val2
+        elif operator is 'divide':
+            return val1/val2
+        elif operator is 'multiply':
+            return val1 * val2
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    calc = Calculator_Class()
+    calc.show()
+    app.exec_()
